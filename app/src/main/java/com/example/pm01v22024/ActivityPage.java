5bda403b4a517ebcc9b78a52ae06d4536cb4fdab
sharoncsh <salazar.sharonc@gmail.com>
2024-02-04 18:14:46 -0600

    se creo la segunda pantalla

---
+++ app/src/main/java/com/example/pm01v22024/ActivityPage.java
@@ -0,0 +1,14 @@
+package com.example.pm01v22024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page);
+    }
+}
